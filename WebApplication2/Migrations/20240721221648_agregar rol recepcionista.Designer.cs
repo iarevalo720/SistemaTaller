53007685b469fec67899932ae361b389a389649c
Israel Arevalo <israelarevalo240@gmail.com>
2024-07-21 19:12:02 -0400

    Se agrega rol recepcionista

--- WebApplication2/Migrations/20240721221648_agregar rol recepcionista.Designer.cs
+++ WebApplication2/Migrations/20240721221648_agregar rol recepcionista.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApplication2.Data;
 
@@ -10,9 +11,11 @@ using WebApplication2.Data;
 namespace WebApplication2.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240721221648_agregar rol recepcionista")]
+    partial class agregarrolrecepcionista
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.7");
---
+++ WebApplication2/Migrations/20240721221648_agregar rol recepcionista.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication2.Migrations
+{
+    /// <inheritdoc />
+    public partial class agregarrolrecepcionista : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "52c04623-8bc1-46e5-8397-da59921724f3", null, "RECEPCIONISTA", "RECEPCIONISTA" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "52c04623-8bc1-46e5-8397-da59921724f3");
+        }
+    }
+}
